7e95fe824c2b7fd9c6a14580ed601c9b4f632903
Alex <Alex@192.168.1.4>
2016-11-10 11:42:38 +0100

    inzializzazione repository

---
+++ pianificatore_ricette.php
@@ -0,0 +1,17 @@
+<?php
+
+//Autore: Alex Vezzelli - Alex Soluzioni Web
+//url: http://www.alexsoluzioniweb.it/
+ /**
+ * @package pianificatore_ricette
+ */
+/*
+Plugin Name: Pianificatore Ricette
+Plugin URI: 
+Description: Plugin personalizzato per la gestione di ingredienti, ricette e la gestione di questi in un'agenda settimanale
+Version: 1.0
+Author: Alex Vezzelli - Alex Soluzioni Web
+Author URI: http://www.alexsoluzioniweb.it/
+License: GPLv2 or later
+*/
+?>
